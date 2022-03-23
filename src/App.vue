9daef245fc3b2e567a125a58b3043a0c9cc9c31f
asickjabarulla <asick.jabarulla@kfzteile24.de>
2022-03-23 09:47:36 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <HeaderNav />
+    <router-view />
+    <Footer />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderNav from "@/components/Header/HeaderNav";
+import Footer from "@/components/Footer/Footer.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderNav,
+    Footer,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import VueAxios from "vue-axios";
+import axios from "axios";
 
-createApp(App).mount('#app')
+const app = createApp(App).use(store).use(router);
+app.mount("#app");
+app.use(VueAxios, axios);
